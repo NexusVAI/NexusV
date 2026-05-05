5fa92de567da0157025781c54947794e2552a3e8
NexusVAI <guxingyu88730882@163.com>
2026-05-05 10:53:19 +0800

    Delete css/后端 directory

--- css/后端/supabase/functions/web-search/index.ts
+++
@@ -1,1 +0,0 @@
-import "./web-search.ts"
--- css/后端/supabase/migrations/20260504010000_add_arena_match_mode.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE public.arena_matches
-  ADD COLUMN IF NOT EXISTS mode TEXT NOT NULL DEFAULT 'anonymous'
-  CHECK (mode IN ('anonymous', 'side_by_side', 'single'));
